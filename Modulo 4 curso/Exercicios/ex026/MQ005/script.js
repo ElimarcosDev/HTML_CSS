da7a999e2c453c3029eefca9f80015392b1d4a4a
Elimarcos De Souza Pereira <110783938+ElimarcosDev@users.noreply.github.com>
2022-09-23 14:29:00 -0300

    Atualizei exercício 003 de media query

--- Modulo 4 curso/Exercicios/ex026/MQ005/script.js
+++ Modulo 4 curso/Exercicios/ex026/MQ005/script.js
@@ -6,3 +6,13 @@ function menuclick(){
      itens.style.display = "block"
     }
  }
+
+ function mudoutamanho(){
+    if (window.innerWidth >= 768){
+        itens.style.display = "block"
+    }
+    else{
+        itens.style.display = "none"
+    }
+    
+ }
